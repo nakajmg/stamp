3baf3e131fae3f3d843e9004e27866199841699e
nakajmg <nakajima.jmg@gmail.com>
2013-08-14 19:22:09 +0900

    ハッカ盆の成果

--- js/stamprally.js
+++ js/stamprally.js
@@ -17,65 +17,110 @@ sr.config(function($routeProvider){
 
 sr.value('experts', {
 	"memberList": [
-		{ "No":  1, "name": "白石 俊平", "link": "shumpei-shiraishi" },
-		{ "No":  2, "name": "小松 健作", "link": "komasshu" },
-		{ "No":  3, "name": "吉川 徹", "link": "yoshikawa_t" },
-		{ "No":  4, "name": "轟 啓介", "link": "keisuke322" },
-		{ "No":  5, "name": "あんどう やすし", "link": "technohippy" },
-		{ "No":  6, "name": "羽田野 太巳", "link": "futomi" },
-		{ "No":  7, "name": "大津 繁樹", "link": "jovi0608" },
-		{ "No":  8, "name": "辻 正浩", "link": "tsuj" },
-		{ "No":  9, "name": "小山田 晃浩", "link": "yomotsu" },
-		{ "No": 10, "name": "斉藤 祐也", "link": "cssradar" },
-		{ "No": 11, "name": "村岡 正和", "link": "bathtimefish" },
-		{ "No": 12, "name": "外村 和仁", "link": "hokaccha" },
-		{ "No": 13, "name": "紀平 拓男", "link": "tkihira" },
-		{ "No": 14, "name": "春日井 良隆", "link": "ykasugai" },
-		{ "No": 15, "name": "物江 修", "link": "osamum_ms" },
-		{ "No": 16, "name": "秋葉 秀樹", "link": "hidetaro7" },
-		{ "No": 17, "name": "城戸 総史", "link": "sou" },
-		{ "No": 18, "name": "矢倉 眞隆", "link": "myakura" },
-		{ "No": 19, "name": "浅井 智也", "link": "dynamitter" },
-		{ "No": 20, "name": "こもり まさあき", "link": "cipher" },
-		{ "No": 21, "name": "Andy Hall", "link": "fenomas" },
-		{ "No": 23, "name": "高橋 登史朗", "link": "toshirot" },
-		{ "No": 24, "name": "鷹野 雅弘", "link": "swwwitch" },
-		{ "No": 25, "name": "清水 俊博", "link": "meso" },
-		{ "No": 26, "name": "古籏 一浩", "link": "openspc" },
-		{ "No": 28, "name": "吾郷 協", "link": "kyo_ago" },
-		{ "No": 29, "name": "浅野 紀予", "link": "noriyo" },
-		{ "No": 30, "name": "Tomomi Imura", "link": "girlie_mac" },
-		{ "No": 31, "name": "Daniel Davis", "link": "ourmaninjapa" },
-		{ "No": 32, "name": "えーじ", "link": "agektmr" },
-		{ "No": 33, "name": "Jxck", "link": "jxck_" },
-		{ "No": 34, "name": "石本 光司", "link": "t32k" },
-		{ "No": 35, "name": "池田 泰延", "link": "clockmaker" },
-		{ "No": 36, "name": "たにぐち まこと", "link": "seltzer" },
-		{ "No": 37, "name": "長谷川恭久", "link": "yhassy" },
-		{ "No": 38, "name": "田中 正裕", "link": "masahiro" },
-		{ "No": 39, "name": "久保田 光則", "link": "anatoo" },
-		{ "No": 40, "name": "木達 一仁", "link": "kazuhito" },
-		{ "No": 43, "name": "増井 雄一郎", "link": "masuidrive" },
-		{ "No": 44, "name": "上村 光星", "link": "kosei27" },
-		{ "No": 45, "name": "比留間 和也", "link": "edo_m18"}
+		{ "No":  1, "name": "白石 俊平", "link": "shumpei-shiraishi", "icon": "./img/1.png","color": "#f04a31" },
+		{ "No":  2, "name": "小松 健作", "link": "komasshu", "icon": "./img/2.jpg","color": "#42bd7b" },
+		{ "No":  3, "name": "吉川 徹", "link": "yoshikawa_t", "icon": "./img/3.jpg","color": "#bd2121" },
+		{ "No":  4, "name": "轟 啓介", "link": "keisuke322", "icon": "./img/4.jpeg","color": "#00ad63" },
+		{ "No":  5, "name": "あんどう やすし", "link": "technohippy", "icon": "./img/5.jpg","color": "#00bd9c" },
+		{ "No":  6, "name": "羽田野 太巳", "link": "futomi", "icon": "./img/6.jpg","color": "#9c5ab5" },
+		{ "No":  7, "name": "大津 繁樹", "link": "jovi0608", "icon": "./img/7.jpg","color": "#5a6b7b" },
+		{ "No":  8, "name": "辻 正浩", "link": "tsuj", "icon": "./img/8.jpg","color": "#de5100" },
+		{ "No":  9, "name": "小山田 晃浩", "link": "yomotsu", "icon": "./img/9.png","color": "#d66352" },
+		{ "No": 10, "name": "斉藤 祐也", "link": "cssradar", "icon": "./img/10.jpg","color": "#009cde" },
+		{ "No": 11, "name": "村岡 正和", "link": "bathtimefish", "icon": "./img/11.jpg","color": "#29ceb4" },
+		{ "No": 12, "name": "外村 和仁", "link": "hokaccha", "icon": "./img/12.jpeg","color": "#314a5a" },
+		{ "No": 13, "name": "紀平 拓男", "link": "tkihira", "icon": "./img/13.jpg","color": "#ffad39" },
+		{ "No": 14, "name": "春日井 良隆", "link": "ykasugai", "icon": "./img/14.png","color": "#c63921" },
+		{ "No": 15, "name": "物江 修", "link": "osamum_ms", "icon": "./img/15.jpg","color": "#ef7b0f" },
+		{ "No": 16, "name": "秋葉 秀樹", "link": "hidetaro7", "icon": "./img/16.jpg","color": "#b57bc6" },
+		{ "No": 17, "name": "城戸 総史", "link": "sou", "icon": "./img/17.jpg","color": "#f76b5a" },
+		{ "No": 18, "name": "矢倉 眞隆", "link": "myakura", "icon": "./img/18.jpg","color": "#5a6b7b" },
+		{ "No": 19, "name": "浅井 智也", "link": "dynamitter", "icon": "./img/19.jpg","color": "#00bd9c" },
+		{ "No": 20, "name": "こもり まさあき", "link": "cipher", "icon": "./img/20.jpg","color": "#00c066" },
+		{ "No": 21, "name": "Andy Hall", "link": "fenomas", "icon": "./img/21.jpeg","color": "#4aade7" },
+		{ "No": 23, "name": "高橋 登史朗", "link": "toshirot", "icon": "./img/22.gif","color": "#de5100" },
+		{ "No": 24, "name": "鷹野 雅弘", "link": "swwwitch", "icon": "./img/24.jpeg","color": "#ff9c00" },
+		{ "No": 25, "name": "清水 俊博", "link": "meso", "icon": "./img/25.jpg","color": "#88bf7e" },
+		{ "No": 26, "name": "古籏 一浩", "link": "openspc", "icon": "./img/26.jpg","color": "#ffce31" },
+		{ "No": 28, "name": "吾郷 協", "link": "kyo_ago", "icon": "./img/28.jpg","color": "#0084bd" },
+		{ "No": 29, "name": "浅野 紀予", "link": "noriyo", "icon": "./img/29.jpg","color": "#c62153" },
+		{ "No": 30, "name": "Tomomi Imura", "link": "girlie_mac", "icon": "./img/30.jpeg","color": "#00bd9c" },
+		{ "No": 31, "name": "Daniel Davis", "link": "ourmaninjapa", "icon": "./img/31.jpg","color": "#f7944a" },
+		{ "No": 32, "name": "えーじ", "link": "agektmr", "icon": "./img/32.jpeg","color": "#695842" },
+		{ "No": 33, "name": "Jxck", "link": "jxck_", "icon": "./img/33.png","color": "#009c85" },
+		{ "No": 34, "name": "石本 光司", "link": "t32k", "icon": "./img/34.jpg","color": "#ff9c00" },
+		{ "No": 35, "name": "池田 泰延", "link": "clockmaker", "icon": "./img/35.png","color": "#42d68c" },
+		{ "No": 36, "name": "たにぐち まこと", "link": "seltzer", "icon": "./img/36.jpeg","color": "#68b7d1" },
+		{ "No": 37, "name": "長谷川恭久", "link": "yhassy", "icon": "./img/37.jpg","color": "#314a5a" },
+		{ "No": 38, "name": "田中 正裕", "link": "masahiro", "icon": "./img/38.jpg","color": "#429cce" },
+		{ "No": 39, "name": "久保田 光則", "link": "anatoo", "icon": "./img/39.jpg","color": "#807666" },
+		{ "No": 40, "name": "木達 一仁", "link": "kazuhito", "icon": "./img/40.jpg","color": "#bd5a7b" },
+		{ "No": 43, "name": "増井 雄一郎", "link": "masuidrive", "icon": "./img/43.jpeg","color": "#bd2121" },
+		{ "No": 44, "name": "上村 光星", "link": "kosei27", "icon": "./img/44.jpg","color": "#de9c63" },
+		{ "No": 45, "name": "比留間 和也", "link": "edo_m18", "icon": "./img/45.jpg","color": "#ffad39"}
 	]
 });
 
 // for memberlist.html
 sr.controller('MemberListCtrl', function($scope, experts){
 	$scope.experts = experts.memberList;
+
+	for(var i = 0, len = experts.memberList.length; i < len; i++){
+		// var $id = $('#stamp-'+experts.memberList[i].link);
+		var skey = experts.memberList[i].link;
+		// console.log(skey);
+		if(window.localStorage.getItem(skey) !== null){
+			var link = experts.memberList[i].link;
+			setTimeout(function(){
+				$('#stamp-'+link).find('section').append('<span class="stamp">');
+			},100);
+		}
+	}
+
 });
 
 // for memberinfo.html
 sr.controller('MemberInfoCtrl', function($scope, $routeParams, experts){
 	var member = {}, link = $routeParams.link;
-	
+
 	for(var i = 0, len = experts.memberList.length; i < len; i++){
 		if(link === experts.memberList[i].link){
 			member = experts.memberList[i];
 			break;
 		}
 	}
-
 	$scope.member = member;
+
+	function pushStamp(){
+		var def = $.Deferred();
+		$('.stamp').remove();
+		var $stamp = $('<div class="stamp anime-stamp">').one('webkitAnimationEnd',function(){
+			$(this).removeClass('anime-stamp');
+			$stamparea.addClass('anime-stampend').one('webkitAnimationEnd',function(){
+				setTimeout(function(){
+					$stamparea.removeClass('anime-stampend');
+				},500);
+				def.resolve();
+			});
+		});
+		$stamparea.append($stamp);
+
+		return def.promise();
+	};
+
+	var $stamparea = $('#info-stamparea');
+	var storage_key = member.link;
+	var storage_value = window.localStorage.getItem(storage_key);
+
+	if(storage_value !== null){
+		$stamparea.append('<div class="stamp">');
+	}else{
+		$stamparea.on('click',function(){
+			var result = window.prompt('Expertコードを入力して下さい');
+			pushStamp().then(function(){
+				window.localStorage.setItem(storage_key,'{ stamped:true}');
+			});
+		});
+	}
+
 });
